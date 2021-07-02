901d51f300cda339e421c70367408a270455378a
Felipe Neuwald <46167104+felipeneuwald@users.noreply.github.com>
2021-07-02 19:28:04 +0100

    update provider

--- fivetran/provider.go
+++ fivetran/provider.go
@@ -8,40 +8,25 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
-// Provider -
 func Provider() *schema.Provider {
 	return &schema.Provider{
 		Schema: map[string]*schema.Schema{
-			"api_key": &schema.Schema{
-				Type:        schema.TypeString,
-				Required:    true,
-				DefaultFunc: schema.EnvDefaultFunc("FIVETRAN_APIKEY", nil),
-			},
-			"api_secret": &schema.Schema{
-				Type:        schema.TypeString,
-				Required:    true,
-				Sensitive:   true,
-				DefaultFunc: schema.EnvDefaultFunc("FIVETRAN_APISECRET", nil),
-			},
+			"api_key":    {Type: schema.TypeString, Required: true, DefaultFunc: schema.EnvDefaultFunc("FIVETRAN_APIKEY", nil)},
+			"api_secret": {Type: schema.TypeString, Required: true, Sensitive: true, DefaultFunc: schema.EnvDefaultFunc("FIVETRAN_APISECRET", nil)},
 		},
 		ResourcesMap: map[string]*schema.Resource{
 			"fivetran_user":  resourceUser(),
 			"fivetran_group": resourceGroup(),
 		},
 		DataSourcesMap: map[string]*schema.Resource{
-			"fivetran_user":  dataSourceUser(),
-			"fivetran_group": dataSourceGroup(),
+			"fivetran_user":        dataSourceUser(),
+			"fivetran_group":       dataSourceGroup(),
+			"fivetran_group_users": dataSourceGroupUsers(),
 		},
 		ConfigureContextFunc: providerConfigure,
 	}
 }
 
 func providerConfigure(ctx context.Context, d *schema.ResourceData) (interface{}, diag.Diagnostics) {
-	var diags diag.Diagnostics
-	apiKey := d.Get("api_key").(string)
-	apiSecret := d.Get("api_secret").(string)
-
-	client := fivetran.New(apiKey, apiSecret)
-
-	return client, diags
+	return fivetran.New(d.Get("api_key").(string), d.Get("api_secret").(string)), diag.Diagnostics{}
 }
