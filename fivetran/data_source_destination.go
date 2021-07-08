7e01b3b1133a99d29b9777d925bcab0f1b3e933f
Felipe Neuwald <46167104+felipeneuwald@users.noreply.github.com>
2021-07-08 19:34:59 +0100

    updata data_source_destination

--- fivetran/data_source_destination.go
+++ fivetran/data_source_destination.go
@@ -3,6 +3,7 @@ package fivetran
 import (
 	"context"
 	"fmt"
+	"strconv"
 
 	"github.com/fivetran/go-fivetran"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
@@ -18,12 +19,11 @@ func dataSourceDestination() *schema.Resource {
 			"service":          {Type: schema.TypeString, Computed: true},
 			"region":           {Type: schema.TypeString, Computed: true},
 			"time_zone_offset": {Type: schema.TypeString, Computed: true},
-			"setup_status":     {Type: schema.TypeString, Computed: true},
 			"config": {Type: schema.TypeSet, Computed: true,
 				Elem: &schema.Resource{
 					Schema: map[string]*schema.Schema{
 						"host":                   {Type: schema.TypeString, Computed: true},
-						"port":                   {Type: schema.TypeString, Computed: true},
+						"port":                   {Type: schema.TypeInt, Computed: true},
 						"database":               {Type: schema.TypeString, Computed: true},
 						"auth":                   {Type: schema.TypeString, Computed: true},
 						"user":                   {Type: schema.TypeString, Computed: true},
@@ -38,13 +38,14 @@ func dataSourceDestination() *schema.Resource {
 						"server_host_name":       {Type: schema.TypeString, Computed: true},
 						"http_path":              {Type: schema.TypeString, Computed: true},
 						"personal_access_token":  {Type: schema.TypeString, Computed: true},
-						"create_external_tables": {Type: schema.TypeBool, Computed: true},
+						"create_external_tables": {Type: schema.TypeString, Computed: true}, // REST API/go-fivetran bool
 						"external_location":      {Type: schema.TypeString, Computed: true},
 						"auth_type":              {Type: schema.TypeString, Computed: true},
 						"role_arn":               {Type: schema.TypeString, Computed: true},
 					},
 				},
 			},
+			"setup_status": {Type: schema.TypeString, Computed: true},
 		},
 	}
 }
@@ -66,8 +67,12 @@ func dataSourceDestinationRead(ctx context.Context, d *schema.ResourceData, m in
 	msi["service"] = resp.Data.Service
 	msi["region"] = resp.Data.Region
 	msi["time_zone_offset"] = resp.Data.TimeZoneOffset
+	config, err := dataSourceDestinationConfig(&resp)
+	if err != nil {
+		return newDiagAppend(diags, diag.Error, "set error", fmt.Sprint(err))
+	}
+	msi["config"] = config
 	msi["setup_status"] = resp.Data.SetupStatus
-	msi["config"] = dataSourceDestinationConfig(&resp)
 	for k, v := range msi {
 		if err := d.Set(k, v); err != nil {
 			return newDiagAppend(diags, diag.Error, "set error", fmt.Sprint(err))
@@ -81,17 +86,23 @@ func dataSourceDestinationRead(ctx context.Context, d *schema.ResourceData, m in
 
 // dataSourceDestinationConfig receives a *fivetran.DestinationDetailsResponse and returns a []interface{}
 // containing the data type accepted by the "config" set.
-func dataSourceDestinationConfig(resp *fivetran.DestinationDetailsResponse) []interface{} {
+func dataSourceDestinationConfig(resp *fivetran.DestinationDetailsResponse) ([]interface{}, error) {
 	var config []interface{}
 
 	c := make(map[string]interface{})
 	c["host"] = resp.Data.Config.Host
-	c["port"] = resp.Data.Config.Port
+	if resp.Data.Config.Port != "" {
+		port, err := strconv.Atoi(resp.Data.Config.Port)
+		if err != nil {
+			return config, err
+		}
+		c["port"] = port
+	}
 	c["database"] = resp.Data.Config.Database
 	c["auth"] = resp.Data.Config.Auth
 	c["user"] = resp.Data.Config.User
 	c["password"] = resp.Data.Config.Password
-	c["connection_type"] = resp.Data.Config.ConnectionType
+	c["connection_type"] = dataSourceDestinationConfigNormalizeConnectionType(resp.Data.Config.ConnectionType)
 	c["tunnel_host"] = resp.Data.Config.TunnelHost
 	c["tunnel_port"] = resp.Data.Config.TunnelPort
 	c["tunnel_user"] = resp.Data.Config.TunnelUser
@@ -101,12 +112,23 @@ func dataSourceDestinationConfig(resp *fivetran.DestinationDetailsResponse) []in
 	c["server_host_name"] = resp.Data.Config.ServerHostName
 	c["http_path"] = resp.Data.Config.HTTPPath
 	c["personal_access_token"] = resp.Data.Config.PersonalAccessToken
-	c["create_external_tables"] = resp.Data.Config.CreateExternalTables
+	if resp.Data.Config.CreateExternalTables != nil {
+		c["create_external_tables"] = boolToStr(*resp.Data.Config.CreateExternalTables)
+	}
 	c["external_location"] = resp.Data.Config.ExternalLocation
 	c["auth_type"] = resp.Data.Config.AuthType
 	c["role_arn"] = resp.Data.Config.RoleArn
 
 	config = append(config, c)
 
-	return config
+	return config, nil
+}
+
+// dataSourceDestinationConfigNormalizeConnectionType normalizes *fivetran.DestinationDetailsResponse.Data.Config.ConnectionType
+// https://fivetran.height.app/T-111758
+func dataSourceDestinationConfigNormalizeConnectionType(connectionType string) string {
+	if connectionType == "SshTunnel" {
+		return "SSHTunnel"
+	}
+	return connectionType
 }
