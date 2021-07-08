31321027aa06639fefa3d82d5466574134186a3b
Felipe Neuwald <46167104+felipeneuwald@users.noreply.github.com>
2021-07-08 19:35:14 +0100

    housekeeping: helpers

--- fivetran/helpers.go
+++ fivetran/helpers.go
@@ -5,8 +5,23 @@ import (
 	"log"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
+
+// strToBool receives a string and returns a boolean
+func strToBool(s string) bool {
+	if s == "true" || s == "TRUE" {
+		return true
+	}
+	return false
+}
+
+// boolToStr receives a boolean and returns a string
+func boolToStr(b bool) string {
+	if b == true {
+		return "true"
+	}
+	return "false"
+}
 
 // newDiag receives a diag.Severity, a summary, a detail, and returns a diag.Diagnostic
 func newDiag(severity diag.Severity, summary, detail string) diag.Diagnostic {
@@ -24,70 +39,8 @@ func newDiagAppend(diags diag.Diagnostics, severity diag.Severity, summary, deta
 	return diags
 }
 
-// set is a temp function. It should be removed, there is no need for it.
-func set(d *schema.ResourceData, kvmap map[string]interface{}) error {
-	for k, v := range kvmap {
-		if err := d.Set(k, v); err != nil {
-			return err
-		}
-	}
-
-	return nil
-}
-
 // debug is a temp function. It should be improved to accept a variadic parameter
 // and its name should change to logDebug
 func debug(v interface{}) {
 	log.Println(fmt.Sprintf("[DEBUG] FIVETRAN: %s", v))
 }
-
-// TEMP STUFF
-//////////////////////////////////////////////////////////////////////////////////////////////
-// // getUserEmail receives users (fivetran.UsersListRespose) and a string id, ranges over the
-// // users and look for a user matching the id. If an id is found, it returns the email address
-// // associated to that user id and a nil error. ............ // should return error if no
-// // user is found?
-// func getUserEmail(users fivetran.UsersListResponse, id string) (string, error) { // should return error?
-// 	for _, user := range users.Data.Items {
-// 		if user.ID == id {
-// 			return user.Email, nil
-// 		}
-// 	}
-
-// 	return "", fmt.Errorf("couldn't find user %v", id) // better error handling
-// }
-
-//////////////////////////////////////////////////////////////////////////
-// func getUsersList(client *fivetran.Client, ctx context.Context) error {
-// 	svc := client.NewUsersList()
-
-// 	var resp fivetran.UsersListResponse
-// 	var respNextCursor string
-// 	limit := 1000
-
-// 	for {
-// 		var err error
-// 		var respInner fivetran.UsersListResponse
-// 		if respNextCursor == "" {
-// 			respInner, err = svc.Limit(limit).Do(ctx)
-// 		}
-// 		if respNextCursor != "" {
-// 			respInner, err = svc.Limit(limit).Cursor(respNextCursor).Do(ctx)
-// 		}
-// 		if err != nil {
-// 			return fmt.Errorf("%v: %v", err, respInner)
-// 		}
-
-// 		for _, item := range respInner.Data.Items {
-// 			resp.Data.Items = append(resp.Data.Items, item)
-// 		}
-
-// 		if respInner.Data.NextCursor == "" {
-// 			break
-// 		}
-
-// 		respNextCursor = respInner.Data.NextCursor
-// 	}
-
-// 	return nil
-// }
