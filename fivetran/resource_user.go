4496f8b955ffb33e83be96c62ec462c4040fcae4
Felipe Neuwald <46167104+felipeneuwald@users.noreply.github.com>
2021-07-21 12:45:22 +0100

    update resource_user

--- fivetran/resource_user.go
+++ fivetran/resource_user.go
@@ -18,7 +18,9 @@ func resourceUser() *schema.Resource {
 		DeleteContext: resourceUserDelete,
 		Importer:      &schema.ResourceImporter{StateContext: schema.ImportStatePassthroughContext},
 		Schema: map[string]*schema.Schema{
-			"id":          {Type: schema.TypeString, Computed: true},
+			"id": {Type: schema.TypeString, Computed: true},
+			// The REST API doesn't provide a method to change the the user's email address.
+			// That's why "ForceNew" is true.
 			"email":       {Type: schema.TypeString, Required: true, ForceNew: true},
 			"given_name":  {Type: schema.TypeString, Required: true},
 			"family_name": {Type: schema.TypeString, Required: true},
@@ -26,7 +28,7 @@ func resourceUser() *schema.Resource {
 			"invited":     {Type: schema.TypeBool, Computed: true},
 			"picture":     {Type: schema.TypeString, Optional: true},
 			"phone":       {Type: schema.TypeString, Optional: true},
-			// "role":         {Type: schema.TypeString, Required: true}, // commented until https://fivetran.height.app/T-109040 is fixed.
+			// "role":         {Type: schema.TypeString, Required: true}, // commented until T-109040 is fixed.
 			"logged_in_at": {Type: schema.TypeString, Computed: true},
 			"created_at":   {Type: schema.TypeString, Computed: true},
 			"last_updated": {Type: schema.TypeString, Optional: true, Computed: true}, // internal
@@ -43,13 +45,13 @@ func resourceUserCreate(ctx context.Context, d *schema.ResourceData, m interface
 	svc.GivenName(d.Get("given_name").(string))
 	svc.FamilyName(d.Get("family_name").(string))
 	if v, ok := d.GetOk("picture"); ok {
-		svc.Picture(v.(string)) // Optional field. Only pass a value if it has been defined
+		svc.Picture(v.(string))
 	}
 	if v, ok := d.GetOk("phone"); ok {
-		svc.Phone(v.(string)) // Optional field. Only pass a value if it has been defined
+		svc.Phone(v.(string))
 	}
 	// The REST API doesn't returns `role` when creating/inviting a new user. Because of that, `role`
-	// is being enforced. This should change when https://fivetran.height.app/T-109040 is fixed.
+	// is being enforced. This should change when T-109040 is fixed.
 	svc.Role("ReadOnly")
 
 	resp, err := svc.Do(ctx)
@@ -100,42 +102,29 @@ func resourceUserUpdate(ctx context.Context, d *schema.ResourceData, m interface
 	var diags diag.Diagnostics
 	client := m.(*fivetran.Client)
 	svc := client.NewUserModify()
-	var change bool
 
-	// // Now using ForceNew: true in the schema.
-	// // Check if ForceNew is the best approach or it is better to keep checking here.
-	// svc.UserID(d.Get("id").(string))
-	// if d.HasChange("email") {
-	// 	return newDiagAppend(diags, diag.Error, "update error", "field email can't be updated")
-	// }
 	if d.HasChange("given_name") {
 		svc.GivenName(d.Get("given_name").(string))
-		change = true
 	}
 	if d.HasChange("family_name") {
 		svc.FamilyName(d.Get("family_name").(string))
-		change = true
 	}
 	if d.HasChange("picture") {
 		svc.Picture(d.Get("picture").(string))
-		change = true
 	}
 	if d.HasChange("phone") {
 		svc.Phone(d.Get("phone").(string))
-		change = true
 	}
 
-	if change {
-		resp, err := svc.Do(ctx)
-		if err != nil {
-			// resourceUserRead here makes sure the state is updated after a NewUserModify error.
-			diags = resourceUserRead(ctx, d, m)
-			return newDiagAppend(diags, diag.Error, "update error", fmt.Sprintf("%v; code: %v; message: %v", err, resp.Code, resp.Message))
-		}
+	resp, err := svc.Do(ctx)
+	if err != nil {
+		// resourceUserRead here makes sure the state is updated after a NewUserModify error.
+		diags = resourceUserRead(ctx, d, m)
+		return newDiagAppend(diags, diag.Error, "update error", fmt.Sprintf("%v; code: %v; message: %v", err, resp.Code, resp.Message))
+	}
 
-		if err := d.Set("last_updated", time.Now().Format(time.RFC850)); err != nil {
-			return newDiagAppend(diags, diag.Error, "set error", fmt.Sprint(err))
-		}
+	if err := d.Set("last_updated", time.Now().Format(time.RFC850)); err != nil {
+		return newDiagAppend(diags, diag.Error, "set error", fmt.Sprint(err))
 	}
 
 	return resourceUserRead(ctx, d, m)
