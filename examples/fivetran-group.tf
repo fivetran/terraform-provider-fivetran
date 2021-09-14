7bac5ee8db21d6a94f0f2190bf49f64ab0555db6
Nick Acosta <30478672+PubChimps@users.noreply.github.com>
2021-09-14 14:10:15 -0700

    Update fivetran-group.tf

--- examples/fivetran-group.tf
+++ examples/fivetran-group.tf
@@ -16,20 +16,6 @@ resource "fivetran_group" "my_new_group_1" {
         role = "ReadOnly"
     }
 
-    # user {
-    #     id = fivetran_user.usertest12.id
-    #     role = "ReadOnly"
-    # }
-
-    # user {
-    #     id = "repaying_tangential"
-    #     role = "ReadOnly"
-    # }
-
-    # user {
-    #     id = "stiffness_grocery"
-    #     role = "ReadOnly"
-    # }
 }
 
 # output "grouptest1_output" {
@@ -41,14 +27,6 @@ resource "fivetran_group" "my_new_group_1" {
 ####################
 
 # ### fivetran_group_connectors
-# data "fivetran_group_connectors" "connector" {
-#     id = "17z306ouk5cey"
-#     schema = "webhooks.salame_audit"
-# }
-
-# output "group_connectors" {
-#     value = data.fivetran_group_connectors.connector
-# }
 
 # ### fivetran_groups
 # data "fivetran_groups" "all" {}
@@ -77,16 +55,4 @@ resource "fivetran_group" "my_new_group_1" {
 #     value = data.fivetran_group_users.my_group_users
 # }
 
-###############
-### IMPORTS ###
-###############
 
-# resource "fivetran_user" "collateral_imputation" {
-#     email        = "felipe.neuwald+apitest999@fivetran.com"
-#     family_name  = "Neuwald API test 999 TERRAFORM"
-#     given_name   = "Felipe"
-# }
-
-# output "user_collateral_imputation" {
-#   value = fivetran_user.collateral_imputation
-# }
