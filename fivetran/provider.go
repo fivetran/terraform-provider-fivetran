5160ef8ee9e41912dfa4723172997ac3c1d781ea
Vitaly Mosin <vitaly.mosin@fivetran.com>
2022-09-13 18:55:18 +0200

    Add 'role', 'is_private_key_encrypted' and 'passphrase' destination config fields

--- fivetran/provider.go
+++ fivetran/provider.go
@@ -9,7 +9,7 @@ import (
 )
 
 var limit = 1000        // REST API response objects limit per HTTP request
-const version = "0.6.2" // Current provider version
+const version = "0.6.4" // Current provider version
 
 func Provider() *schema.Provider {
 	return &schema.Provider{
