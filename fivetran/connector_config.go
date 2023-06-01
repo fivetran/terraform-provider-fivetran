a0c9ceb49adcba8252c2f9852b094d1d08a6b410
Luka Devic <43738975+LukaDevic@users.noreply.github.com>
2023-06-01 16:26:13 +0200

    TPF-T-467339 refactor: review

--- fivetran/connector_config.go
+++ fivetran/connector_config.go
@@ -63,6 +63,52 @@ func connectorSchemaAuth() *schema.Schema {
 	}
 }
 
+// connectorReadConfig receives a *fivetran.ConnectorDetailsResponse and returns a []interface{}
+// containing the data type accepted by the "config" list.
+func getConnectorReadCustomConfig(resp *fivetran.ConnectorCustomDetailsResponse, currentConfigs *[]interface{}) []interface{} {
+	config := make(map[string]interface{})
+	originConfig := make(map[string]interface{})
+	if currentConfigs != nil && len(*currentConfigs) > 0 {
+		originConfig = (*currentConfigs)[0].(map[string]interface{})
+	}
+
+	originConfig = populateOriginConfigFromResponse(originConfig, resp.Data.Config)
+
+	properties := getProperties()
+
+	for property, propertySchema := range properties {
+		if propertySchema.Type == schema.TypeSet || propertySchema.Type == schema.TypeList {
+			if values, ok := originConfig[property].([]string); ok {
+				config[property] = xStrXInterface(values)
+				continue
+			}
+
+			if interfaceValues, ok := originConfig[property].([]interface{}); ok && len(interfaceValues) > 0 {
+				if _, ok := interfaceValues[0].(map[string]interface{}); ok {
+					config[property] = interfaceValues
+				} else {
+					config[property] = xInterfaceStrXStr(interfaceValues)
+				}
+				continue
+			}
+		}
+		if value, ok := originConfig[property].(string); ok && value != "" {
+			switch propertySchema.Type {
+			case schema.TypeBool:
+				config[property] = strToBool(value)
+			case schema.TypeInt:
+				config[property] = strToInt(value)
+			default:
+				config[property] = value
+			}
+		}
+	}
+	configs := make([]interface{}, 1)
+	configs[0] = config
+
+	return configs
+}
+
 func populateOriginConfigFromResponse(originConfig map[string]interface{}, responseConfig map[string]interface{}) map[string]interface{} {
 	for responseProperty, value := range responseConfig {
 		if responseProperty == "project_credentials" && originConfig[responseProperty] != nil {
@@ -79,16 +125,6 @@ func populateOriginConfigFromResponse(originConfig map[string]interface{}, respo
 			if isMaskedValue(valueArray, originConfig, responseProperty) {
 				continue
 			}
-			// if valueMap, ok := valueArray[0].(map[string]interface{}); ok {
-			// 	if valueMap["value"] == MASKED_VALUE && originConfig[responseProperty] != nil {
-			// 		continue
-			// 	}
-			// }
-			// if valueStringArray, ok := valueArray[0].([]string); ok {
-			// 	if valueStringArray[0] == MASKED_VALUE {
-			// 		continue
-			// 	}
-			// }
 			originConfig[responseProperty] = valueArray
 			continue
 		}
@@ -111,63 +147,4 @@ func isMaskedValue(valueArray []interface{}, originConfig map[string]interface{}
 		}
 	}
 	return false
-}
-
-// connectorReadConfig receives a *fivetran.ConnectorDetailsResponse and returns a []interface{}
-// containing the data type accepted by the "config" list.
-func getConnectorReadCustomConfig(resp *fivetran.ConnectorCustomDetailsResponse, currentConfigs *[]interface{}) []interface{} {
-	configs := make([]interface{}, 1)
-
-	config := make(map[string]interface{})
-	originConfig := make(map[string]interface{})
-	if currentConfigs != nil && len(*currentConfigs) > 0 {
-		originConfig = (*currentConfigs)[0].(map[string]interface{})
-	}
-
-	originConfig = populateOriginConfigFromResponse(originConfig, resp.Data.Config)
-
-	properties := getProperties()
-
-	for property, propertySchema := range properties {
-
-		if _, ok := sensitiveFields[property]; ok {
-			if v, ok := originConfig[property].(string); ok {
-				mapAddStr(config, property, v)
-			}
-			if v, ok := originConfig[property].([]interface{}); ok {
-				mapAddXInterface(config, property, v)
-			}
-			continue
-		}
-
-		if propertySchema.Type == schema.TypeSet || propertySchema.Type == schema.TypeList {
-			if values, ok := originConfig[property].([]string); ok {
-				config[property] = xStrXInterface(values)
-				continue
-			}
-
-			if interfaceValues, ok := originConfig[property].([]interface{}); ok && len(interfaceValues) > 0 {
-				if _, ok := interfaceValues[0].(map[string]interface{}); ok {
-					config[property] = interfaceValues
-				} else {
-					config[property] = xInterfaceStrXStr(interfaceValues)
-				}
-				continue
-			}
-		}
-		if value, ok := originConfig[property].(string); ok && value != "" {
-			valueType := propertySchema.Type
-			switch valueType {
-			case schema.TypeBool:
-				config[property] = strToBool(value)
-			case schema.TypeInt:
-				config[property] = strToInt(value)
-			default:
-				config[property] = value
-			}
-		}
-	}
-	configs[0] = config
-
-	return configs
 }
