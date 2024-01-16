34e987e3b606f4aa22d9e7f202b6441f1fba85ee
beevital <vitaly.mosin@fivetran.com>
2024-01-16 16:36:38 +0100

    Fix race conditions in connector resource

--- fivetran/framework/core/schema/connector_auth.go
+++ fivetran/framework/core/schema/connector_auth.go
@@ -1,6 +1,8 @@
 package schema
 
 import (
+	"sync"
+
 	"github.com/fivetran/terraform-provider-fivetran/fivetran/common"
 	resourceSchema "github.com/hashicorp/terraform-plugin-framework/resource/schema"
 )
@@ -8,30 +10,41 @@ import (
 var (
 	authResourceSchemaAttrs  map[string]resourceSchema.Attribute
 	authResourceSchemaBlocks map[string]resourceSchema.Block
+
+	authResourceSchemaAttrsMutex  sync.RWMutex = sync.RWMutex{}
+	authResourceSchemaBlocksMutex sync.RWMutex = sync.RWMutex{}
 )
 
 func GetResourceAuthSchemaAttributes() map[string]resourceSchema.Attribute {
 	if len(authResourceSchemaAttrs) == 0 {
-		authResourceSchemaAttrs = make(map[string]resourceSchema.Attribute)
+		result := make(map[string]resourceSchema.Attribute)
 		for fn, f := range common.GetAuthFieldsMap() {
 			attr := schemaAttributeFromConfigField(f, false).(resourceSchema.Attribute)
 			if f.FieldValueType != common.ObjectList && f.FieldValueType != common.Object {
-				authResourceSchemaAttrs[fn] = attr
+				result[fn] = attr
 			}
 		}
+		if authResourceSchemaAttrsMutex.TryLock() {
+			authResourceSchemaAttrs = result
+		}
+		return result
 	}
 	return authResourceSchemaAttrs
 }
 
 func GetResourceAuthSchemaBlocks() map[string]resourceSchema.Block {
 	if len(authResourceSchemaBlocks) == 0 {
-		authResourceSchemaBlocks = make(map[string]resourceSchema.Block)
+		result := make(map[string]resourceSchema.Block)
 		for fn, f := range common.GetAuthFieldsMap() {
 			attr := schemaBlockFromConfigField(f)
 			if f.FieldValueType == common.ObjectList || f.FieldValueType == common.Object {
-				authResourceSchemaBlocks[fn] = attr
+				result[fn] = attr
 			}
 		}
+		if authResourceSchemaBlocksMutex.TryLock() {
+			authResourceSchemaBlocks = result
+		}
+		return result
 	}
 	return authResourceSchemaBlocks
 }
