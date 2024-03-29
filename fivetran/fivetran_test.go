d4f15104d8dc0f59812e6e3913d1c209fdb68e57
beevital <vitaly.mosin@fivetran.com>
2024-03-29 14:44:33 +0100

    tests fix #4

--- fivetran/fivetran_test.go
+++ fivetran/fivetran_test.go
@@ -16,7 +16,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-mux/tf6muxserver"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	"github.com/hashicorp/terraform-plugin-testing/terraform"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 )
 
 const (
--- fivetran/resource_dbt_transformation_test.go
+++ fivetran/resource_dbt_transformation_test.go
@@ -6,8 +6,8 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
-	"github.com/hashicorp/terraform-plugin-testing/terraform"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 )
 
 func TestResourceDbtTransformationE2E(t *testing.T) {
