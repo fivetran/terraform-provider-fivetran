1667656bbc6f55f93c95fe1e4add6ff7620cf753
beevital <vitaly.mosin@fivetran.com>
2024-04-18 17:54:08 +0200

    Release version

--- fivetran/framework/provider.go
+++ fivetran/framework/provider.go
@@ -17,7 +17,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/types"
 )
 
-const Version = "1.1.21" // Current provider version
+const Version = "1.1.22" // Current provider version
 
 type fivetranProvider struct {
 	mockClient httputils.HttpClient
