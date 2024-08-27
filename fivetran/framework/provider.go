ad328c5fe77625155a0d847cd0e13a93c7e8eeae
jovan.manojlovic <jovan.manojlovic@fivetran.com>
2024-08-27 12:07:10 +0200

    Fix versions

--- fivetran/framework/provider.go
+++ fivetran/framework/provider.go
@@ -17,7 +17,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/types"
 )
 
-const Version = "1.2.7" // Current provider version
+const Version = "1.2.8" // Current provider version
 
 type fivetranProvider struct {
 	mockClient httputils.HttpClient
