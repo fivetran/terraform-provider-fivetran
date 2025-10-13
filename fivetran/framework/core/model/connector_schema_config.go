b1309f6dd7c491996185a3dc1e8d8673cc8e993f
jovan.manojlovic <jovan.manojlovic@fivetran.com>
2025-10-13 13:03:10 +0200

    Fix is_primary_key default handling to prevent null values causing plan diffs
    - Default is_primary_key to false when API doesn't provide it
    - Prevents null values from showing as plan changes
    - Updated version to 1.9.10
    - Updated CHANGELOG with additional context

--- fivetran/framework/core/model/connector_schema_config.go
+++ fivetran/framework/core/model/connector_schema_config.go
@@ -266,10 +266,11 @@ func (d *ConnectorSchemaResourceModel) getSchemasMap(schemas []interface{}) base
 						}
 
 						// is_primary_key is computed-only, always populate from API response
+						// Default to false if not provided by API
 						if columnMap["is_primary_key"] != nil {
 							columnElements["is_primary_key"] = types.BoolValue(helpers.StrToBool(columnMap["is_primary_key"].(string)))
 						} else {
-							columnElements["is_primary_key"] = types.BoolNull()
+							columnElements["is_primary_key"] = types.BoolValue(false)
 						}
 						columnValue, _ := types.ObjectValue(columnsAttrTypes, columnElements)
 						columns[columnName] = columnValue
@@ -385,10 +386,11 @@ func (d *ConnectorSchemaResourceModel) getLegacySchemaItems(schemas []interface{
 							columnElements["hashed"] = types.BoolNull()
 						}
 						// is_primary_key is computed-only, always populate from API response
+						// Default to false if not provided by API
 						if columnMap["is_primary_key"] != nil {
 							columnElements["is_primary_key"] = types.BoolValue(helpers.StrToBool(columnMap["is_primary_key"].(string)))
 						} else {
-							columnElements["is_primary_key"] = types.BoolNull()
+							columnElements["is_primary_key"] = types.BoolValue(false)
 						}
 						columnValue, _ := types.ObjectValue(columnAttrTypes, columnElements)
 						columns = append(columns, columnValue)
